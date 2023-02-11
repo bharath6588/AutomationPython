b34524fa38aec4520bf9bbb444b7689c18344c2f
Bharath M <bharathm@Bharaths-MacBook-Air.local>
2023-02-11 23:54:22 +0530

    updated

--- sdbqawithdbapi/src/helpers/config_helpers.py
+++ sdbqawithdbapi/src/helpers/config_helpers.py
@@ -7,8 +7,8 @@ def get_base_url():
     env = os.environ.get('ENV', 'test')  # telling if none of the environment is set and simply run the test in test environment
 
     if env.lower() == 'test':
-        #return 'http://demostore.supersqa.com'
-        return 'http://localhost:10003/shop/'
+        return 'http://demostore.supersqa.com'
+        #return 'http://localhost:10003/shop/'
     elif env.lower() == 'prod':
         return 'http://demostore.prod.supersqa.com'
     elif env.lower() == 'testw':
